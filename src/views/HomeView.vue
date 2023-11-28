e0e09c8c40d02d3ace233baffc3f4fc0b1f84002
Ian Cenry <ian.c@nathandigital.com>
2023-11-28 10:08:10 +0300

    bug: fix suspense

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,28 @@
 <script setup>
 import { storeToRefs } from 'pinia';
-import Card from '../components/CardComp.vue';
+import { onMounted } from 'vue';
+
 import { useMemoryStore } from '../stores/memory';
+
+import Card from '../components/CardComp.vue';
+// const Card = defineAsyncComponent(() => import('../components/CardComp.vue'));
 
 const memoryStore = useMemoryStore();
 
 const { memories } = storeToRefs(memoryStore);
-memoryStore.getMemories();
+onMounted(() => {
+  memoryStore.getMemories();
+});
 </script>
 
 <template>
   <section class="grid grid-cols-3 gap-10">
-    <Card v-for="memory in memories" :key="memory.id" :memory="memory" />
+    <Suspense>
+      <template #default>
+        <Card v-for="memory in memories" :key="memory.id" :memory="memory" />
+      </template>
+
+      <template #fallback> Loading... </template>
+    </Suspense>
   </section>
 </template>
